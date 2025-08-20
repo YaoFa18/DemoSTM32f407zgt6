2a2f01cb64a57bf2fa7172e6e094b88e834b2489
YaoFa18 <w2959316208@gmail.com>
2025-08-20 17:49:04 +0800

    First correction

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -51,7 +51,7 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOF, LED0_Pin|LED1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOF, LED0_Pin|LED1_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin : LED0_Pin */
   GPIO_InitStruct.Pin = LED0_Pin;
